914c854eb76a358581f34416b72e98422e759df3
erasaniabhinav <abhinavreddy0909@gmail.com>
2024-05-01 10:36:14 -0500

    add_update

--- python/star_pattern.py
+++ python/star_pattern.py
@@ -10,7 +10,7 @@ def print_star(n):
         #iterates again
         for j in range(i+1):
             #we print * and then give a space (executes in python3)
-            print("*",end = ' ')
+            print("*",end=" ")
 
         #this will print a new line    
         print()
